b5d46774f05e9d5df4c89e46dd1abb7c3c013ebe
MElkmeshi <mohamed2108926@miuegypt.edu.eg>
2023-05-09 17:24:12 +0300

    add products page and product page

---
+++ products.js
@@ -0,0 +1,80 @@
+export const products = [
+  {
+    name: "Nike Slim shirt",
+    slug: "nike-slim-shirt",
+    category: "Shirts",
+    image:
+      "https://drive.google.com/uc?export=download&id=1DP-sRfu_12Y2MAA3EEz24nHDBlhpmUjw", // 679px × 829px
+    price: 120,
+    countInStock: 10,
+    brand: "Nike",
+    rating: 4.5,
+    numReviews: 10,
+    description: "high quality shirt",
+  },
+  {
+    name: "Adidas Fit Shirt",
+    slug: "adidas-fit-shirt",
+    category: "Shirts",
+    image:
+      "https://drive.google.com/uc?export=download&id=1elK-iWVvFqJ3fyQxq42ccTto7B5gc32F",
+    price: 250,
+    countInStock: 20,
+    brand: "Adidas",
+    rating: 4.0,
+    numReviews: 10,
+    description: "high quality product",
+  },
+  {
+    name: "Nike Slim shirt",
+    slug: "nike-slim-shirt",
+    category: "Shirts",
+    image:
+      "https://drive.google.com/uc?export=download&id=1DP-sRfu_12Y2MAA3EEz24nHDBlhpmUjw", // 679px × 829px
+    price: 120,
+    countInStock: 10,
+    brand: "Nike",
+    rating: 4.5,
+    numReviews: 10,
+    description: "high quality shirt",
+  },
+  {
+    name: "Nike Slim Pant",
+    slug: "nike-slim-pant",
+    category: "Pants",
+    image:
+      "https://drive.google.com/uc?export=download&id=150kP5X4mx85U_7UX9HkxHG2Exch0TGbM",
+    price: 25,
+    countInStock: 15,
+    brand: "Nike",
+    rating: 3.5,
+    numReviews: 14,
+    description: "high quality product",
+  },
+  {
+    name: "Adidas Fit Pant",
+    slug: "adidas-fit-pant",
+    category: "Pants",
+    image:
+      "https://drive.google.com/uc?export=download&id=1JoI8X19Z1tj6YYqaP-A1QSqnhQmRZb6d",
+    price: 65,
+    countInStock: 5,
+    brand: "Puma",
+    rating: 2.5,
+    numReviews: 10,
+    description: "high quality product",
+  },
+  {
+    name: "Adidas Fit Pant",
+    slug: "adidas-fit-pant",
+    category: "Pants",
+    image:
+      "https://drive.google.com/uc?export=download&id=1JoI8X19Z1tj6YYqaP-A1QSqnhQmRZb6d",
+    price: 65,
+    countInStock: 5,
+    brand: "Puma",
+    rating: 2.5,
+    numReviews: 10,
+    description: "high quality product",
+  },
+];
--- server.js
+++ server.js
@@ -1,9 +1,8 @@
 import express from "express";
-import ejs from "ejs";
-
 import path from "path";
-import fs from "fs";
 import { fileURLToPath } from "url";
+import ejs from "ejs";
+import { products } from "./products.js";
 
 //Read the current directory name
 const __filename = fileURLToPath(import.meta.url);
@@ -13,48 +12,47 @@ const hostname = "127.0.0.1";
 const port = 3000;
 const app = express();
 
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 //app.use(express.static('public'));
-app.use('/public', express.static(__dirname + '/public'));
+app.use("/public", express.static(__dirname + "/public"));
 //app.set('views', path.join(__dirname, '../views'));
 
+app.get("/products", function (req, res) {
+  res.render("pages/products", { products });
+});
+app.get("/product/:slug", function (req, res) {
+  const product = products.find((p) => p.slug === req.params.slug);
+  res.render("pages/product", { product });
+});
 
-
-
-
-
-
-app.get('/', function(req, res) {
-  // let data = { 
-  //   name: 'Yahia Khalid', 
-  //   age: 21, 
+app.get("/", function (req, res) {
+  // let data = {
+  //   name: 'Yahia Khalid',
+  //   age: 21,
   //   city: 'Cairo',
   //   favorite_food: 'Pizza'
   // };
   console.log("Rendering index page...");
-  res.render('pages/index');
+  res.render("pages/index");
 });
 
-app.get('/laaptop', function(req, res) {
-
-  res.render('/pages/laaptop');
+app.get("/laaptop", function (req, res) {
+  res.render("/pages/laaptop");
 });
 
-app.get('/phhones', function(req, res) {
+app.get("/phhones", function (req, res) {
   // let browsers = [
   //   { name: 'chrome', org: "Google", share: "62%"},
   //   { name: 'Safari', org: "Apple", share: "24%"},
   //   { name: 'Edge', org: "Microsoft", share: "5%"}
   // ];
-  res.render('/pages/phhones');
+  res.render("/pages/phhones");
 
   let tagline = "Browsers and their market share";
-  res.render('pages/loop', { browsers: browsers, tagline: tagline });
-
+  res.render("pages/loop", { browsers: browsers, tagline: tagline });
 });
 
-
 app.listen(3000, () => {
-    console.log(`Server running at http://${hostname}:${port}/ejs-test`);
-    console.log(`Server running at local dirname :  ${__dirname})  `);
-  });
+  console.log(`Server running at http://${hostname}:${port}/ejs-test`);
+  console.log(`Server running at local dirname :  ${__dirname})  `);
+});
