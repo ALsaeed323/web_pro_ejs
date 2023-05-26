566c54a639495d7a5b6db01d068a824546602edb
Alsaead Aziz <alsaead2110679@miuegypt.edu.eg>
2023-05-26 11:20:46 +0300

    try to handle the vaildation in database

--- models/userModel.js
+++ models/userModel.js
@@ -22,4 +22,31 @@ const User = mongoose.model(
     }
   )
 );
+User.pre("save", function(next) {
+  const user = this;
+
+  // Only hash the password if it has been modified (or is new)
+  if (!user.isModified('password')) return next();
+
+  // Generate a salt
+  bcrypt.genSalt(SALT_WORK_FACTOR, function(err, salt) {
+    if (err) return next(err);
+
+    // Hash the password using the generated salt
+    bcrypt.hash(user.password, salt, function(err, hash) {
+      if (err) return next(err);
+
+      // Override the cleartext password with the hashed one
+      user.password = hash;
+      next();
+    });
+  });
+});
+
+User.methods.comparePassword = function(candidatePassword, cb) {
+  bcrypt.compare(candidatePassword, this.password, function(err, isMatch) {
+    if (err) return cb(err);
+    cb(null, isMatch);
+  });
+};
 export default User;
