a19aa378f3b5619c4cdc056694b314359a133b2c
Alsaead Aziz <alsaead2110679@miuegypt.edu.eg>
2023-05-26 11:24:09 +0300

    again try to handle the vaildation in database

--- models/userModel.js
+++ models/userModel.js
@@ -1,28 +1,29 @@
 import mongoose from "mongoose";
+import bcrypt from "bcrypt";
 
-const User = mongoose.model(
-  "User",
-  new mongoose.Schema(
-    {
-      name: { type: String, required: true },
-      email: { type: String, required: true, unique: true },
-      password: { type: String, required: true },
-      isAdmin: { type: Boolean, default: false, required: true },
-      shippingAddress: {
-        fullName: { type: String, required: false },
-        address: { type: String, required: false },
-        city: { type: String, required: false },
-        postalCode: { type: String, required: false },
-        country: { type: String, required: false },
-      },
-      paymentMethod: { type: String, required: false },
+const SALT_WORK_FACTOR = 10;
+
+const userSchema = new mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    isAdmin: { type: Boolean, default: false, required: true },
+    shippingAddress: {
+      fullName: { type: String, required: false },
+      address: { type: String, required: false },
+      city: { type: String, required: false },
+      postalCode: { type: String, required: false },
+      country: { type: String, required: false },
     },
-    {
-      timestamps: true,
-    }
-  )
+    paymentMethod: { type: String, required: false },
+  },
+  {
+    timestamps: true,
+  }
 );
-User.pre("save", function(next) {
+
+userSchema.pre("save", function(next) {
   const user = this;
 
   // Only hash the password if it has been modified (or is new)
@@ -43,10 +44,13 @@ User.pre("save", function(next) {
   });
 });
 
-User.methods.comparePassword = function(candidatePassword, cb) {
+userSchema.methods.comparePassword = function(candidatePassword, cb) {
   bcrypt.compare(candidatePassword, this.password, function(err, isMatch) {
     if (err) return cb(err);
     cb(null, isMatch);
   });
 };
+
+const User = mongoose.model("User", userSchema);
+
 export default User;
